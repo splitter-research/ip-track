ec01ab4311fd963a1aec35beeb67b087a7c9c73b
nuc2 <splitter-research@protonmail.com>
2021-12-08 23:23:01 +0000

    auto-commit Wed 08 Dec 2021 11:23:01 PM UTC

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ifconfig > /home/nuc/ip-track/ip.txt
+ifconfig > /home/nuc/ip-track/nuc2.txt
 git -C "/home/nuc/ip-track" add -A
 git -C "/home/nuc/ip-track" commit -m "auto-commit $(date)"
 git -C "/home/nuc/ip-track" push -u origin master 
